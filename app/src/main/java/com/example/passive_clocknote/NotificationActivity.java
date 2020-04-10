c2d79cca0560479c930f49b2ad6bbcc8cd3c939f
Neha Rani <45110927+neha1010@users.noreply.github.com>
2020-04-10 17:52:25 -0400

    alarm receiver for notification

---
+++ app/src/main/java/com/example/passive_clocknote/NotificationActivity.java
@@ -0,0 +1,11 @@
+package com.example.passive_clocknote;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
